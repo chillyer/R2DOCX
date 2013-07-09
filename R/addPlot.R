56ea8d9816cf9503ba3b4d52e8c3591825a85beb
chillyer <hillyer@engr.orst.edu>
2013-07-09 14:23:24 -0700

    Update addPlot.R
    added other arguments to grDevices:png call

--- R/addPlot.R
+++ R/addPlot.R
@@ -3,12 +3,19 @@
 # Author: David GOHEL <david.gohel@lysis-consultants.fr>
 # Date: 1 avr. 2013
 # Version: 0.1
+# July 9, 2013; added other arguments to grDevices:png call -cch
 ###############################################################################
 
 setMethod("addPlot", "Docx", function(doc, fun
 		, legend, stylename = "PlotReference"
 		, width = 7, height = 6
 		, pointsize = 12
+                , units = "in"
+                , bg = "white"
+                , res = 72
+                , family = ""
+                , type = c("windows", "cairo", "cairo-png")
+		
 		, visible = T, ... ) {
 			if( !missing( legend) && legend != "" ){
 				# check style does exist
@@ -22,8 +29,15 @@ setMethod("addPlot", "Docx", function(doc, fun
 			filename = paste( dirname, "/plot%03d.png" ,sep = "" )
 
 			grDevices::png (filename = filename
-					, width = width*72.2, height = height*72.2
+					, width = width, height = height
 					, pointsize = pointsize
+					, units = units
+                                        , bg = bg
+                                        , res = res
+                                        , family = family
+                                        , type = type
+                                        #antialias is not included b/c it can be specified by changing windows.options()$bitmap.aa.win
+                                        #restoreConsole is not included b/c it isn't relevant in this context
 			)
 			
 			fun(...)
